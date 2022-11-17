cb8b6b8d8c3467a4c159bb71ae7e802604bc8d2a
Naiara Borges <naiarazk@gmail.com>
2022-11-17 19:47:13 -0300

    Criando models e router

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,5 +1,14 @@
-let text: string;
+/* eslint-disable linebreak-style */
+import express from 'express';
+import mongoose from 'mongoose';
 
-text = '123';
+mongoose.connect('mongodb://localhost:27017')
+  .then(() => {
+    const app = express();
+    const port = 3001;
 
-console.log(text);
+    app.listen(port, () => {
+      console.log(`🚀 Server is running on http://localhost:${port}`);
+    });
+  })
+  .catch(() => console.log('Erro ao conectar no mongodb'));
